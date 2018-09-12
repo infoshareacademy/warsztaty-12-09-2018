a0936fad794b68c75505e706ea3054aad0ade4c3
Mateusz Choma <chomamateusz@gmail.com>
2018-09-12 22:47:37 +0200

    Wrapped in window on load

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
+window.addEventListener(
+  'load',
+  () => {
+    const makeBlackSquare = (x, y) => {
+      const div = document.createElement('div')
 
-const makeBlackSquare = (x, y) => {
-  const div = document.createElement('div')
+      div.classList.add('square')
+      div.classList.add('square--black')
 
-  div.classList.add('square')
-  div.classList.add('square--black')
+      div.style.left = x + 'px'
+      div.style.top = y + 'px'
 
-  div.style.left = x + 'px'
-  div.style.top = y + 'px'
+      document.body.appendChild(div)
+    }
 
-  document.body.appendChild(div)
-}
+    const render = (positionsOfSquares) => {
+      if (!positionsOfSquares) return
 
-const render = (positionsOfSquares) => {
-  if (!positionsOfSquares) return
+      document.body.innerHTML = ''
 
-  document.body.innerHTML = ''
+      positionsOfSquares.forEach((position) => {
+        makeBlackSquare(position.x, position.y)
+      })
+    }
 
-  positionsOfSquares.forEach((position) => {
-    makeBlackSquare(position.x, position.y)
-  })
-}
+    const dbRef = firebase.database().ref('/squares')
 
-const dbRef = firebase.database().ref('/squares')
+    document.body.addEventListener(
+      'click',
+      (event) => {
+        dbRef.push(
+          { x: event.pageX, y: event.pageY }
+        )
 
-document.body.addEventListener(
-  'click',
-  (event) => {
-    dbRef.push(
-      { x: event.pageX, y: event.pageY }
+        render()
+      }
     )
 
-    render()
-  }
-)
-
-dbRef.on(
-  'value',
-  (snapshot) => {
-    const positionsOfSquares = Object.values(snapshot.val())
-    render(positionsOfSquares)
+    dbRef.on(
+      'value',
+      (snapshot) => {
+        const positionsOfSquares = Object.values(snapshot.val())
+        render(positionsOfSquares)
+      }
+    )
   }
 )
