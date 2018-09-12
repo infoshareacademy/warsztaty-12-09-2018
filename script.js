5c43241b4b5d23438e044b8a9b64c39754fe636c
Mateusz Choma <chomamateusz@gmail.com>
2018-09-12 20:55:50 +0200

    makeBlackSquare with position  form arguments

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const div = document.createElement('div')
-div.classList.add('square')
-div.classList.add('square--black')
+const makeBlackSquare = (x, y) => {
+  const div = document.createElement('div')
 
-document.body.appendChild(div)
+  div.classList.add('square')
+  div.classList.add('square--black')
+
+  div.style.left = x + 'px'
+  div.style.top = y + 'px'
+
+  document.body.appendChild(div)
+}
+
+makeBlackSquare(200, 100)
+makeBlackSquare(300, 100)
 
